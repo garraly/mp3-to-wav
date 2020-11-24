f07d35429b28ab5234d719629bd5d18de451a46a
garra <892874193@qq.com>
2020-11-24 20:32:48 +0800

    适配windows

--- index.js
+++ index.js
@@ -7,65 +7,65 @@ const path = require('path')
 
 class Mp32Wav {
 
-  constructor(input_file_path, output_dir) {
+    constructor(input_file_path, output_dir) {
 
-    if (!utils.checkArgsNotNull(...input_file_path)) {
-      throw new Error('err arguments')
-    }
-    output_dir = utils.judgeNotNull(output_dir) ? output_dir : utils.splitFileDir(input_file_path)
-    this._input_file_path = input_file_path
-    this._input_file_name = utils.splitFilename(input_file_path)
-    this._output_dir = output_dir
-    this._output_file_name = this._input_file_name.toString().replace(/\.mp3/i, '')
-  }
-
-  async exec() {
-
-    try {
-      const mp3DecodeRes = await this.decodeMp3(this._input_file_path)
-      const wavPath = this.saveForWav(mp3DecodeRes.data, this._output_dir, this._output_file_name, mp3DecodeRes.sampleRate, mp3DecodeRes.channels, mp3DecodeRes.float)
-      console.log(`Mp32Wav convert to wav file successfully, saving on: ${wavPath}`)
-    } catch (err) {
-      console.error(`mp3 to wav exec err: ${err.message}`)
-    }
-  }
-
-  async decodeMp3(file_path) {
-
-    try {
-      let buffer = utils.readFile(file_path)
-      return new Promise(async resolve => {
-        const audioBuffer = await mp3Decode(buffer)
-        const channelData = []
-        for (let i = 0; i < audioBuffer.numberOfChannels; i++) {
-          channelData[i] = audioBuffer.getChannelData(i)
+        if (!utils.checkArgsNotNull(...input_file_path)) {
+            throw new Error('err arguments')
         }
-        return resolve({
-          data: channelData,
-          sampleRate: audioBuffer.sampleRate,
-          channels: audioBuffer.numberOfChannels,
-          float: audioBuffer.floatingPoint
-        })
-      })
-    } catch (err) {
-      throw new Excetpion(`decode mp3 err: ${err.message}`)
+        output_dir = utils.judgeNotNull(output_dir) ? output_dir : utils.splitFileDir(input_file_path)
+        this._input_file_path = input_file_path
+        this._input_file_name = utils.splitFilename(input_file_path)
+        this._output_dir = output_dir
+        this._output_file_name = this._input_file_name.toString().replace(/\.mp3/i, '')
     }
-  }
 
-  saveForWav(buffer, savePath, filename = '', sampleRate, channels, float = true) {
+    async exec() {
 
-    if (!filename) filename = 'temp-' + utils.generateTimestampRandom()
-    const fileFullName = filename + '.wav'
-    const fileFullPath = path.join(savePath, fileFullName)
-
-    try {
-      const wavData = wav.encode(buffer, {sampleRate: sampleRate, float: float, channels: channels})
-      utils.saveToPath(fileFullPath, wavData)
-      return fileFullPath
-    } catch (err) {
-      throw new Error(`saveForWav err: ${err.message}`)
+        try {
+            const mp3DecodeRes = await this.decodeMp3(this._input_file_path)
+            const wavPath = this.saveForWav(mp3DecodeRes.data, this._output_dir, this._output_file_name, mp3DecodeRes.sampleRate, mp3DecodeRes.channels, mp3DecodeRes.float)
+            console.log(`Mp32Wav convert to wav file successfully, saving on: ${wavPath}`)
+        } catch (err) {
+            console.error(`mp3 to wav exec err: ${err.message}`)
+        }
     }
-  }
+
+    async decodeMp3(file_path) {
+
+        try {
+            let buffer = utils.readFile(file_path)
+            return new Promise(async resolve => {
+                const audioBuffer = await mp3Decode(buffer)
+                const channelData = []
+                for (let i = 0; i < audioBuffer.numberOfChannels; i++) {
+                    channelData[i] = audioBuffer.getChannelData(i)
+                }
+                return resolve({
+                    data: channelData,
+                    sampleRate: audioBuffer.sampleRate,
+                    channels: audioBuffer.numberOfChannels,
+                    float: audioBuffer.floatingPoint
+                })
+            })
+        } catch (err) {
+            throw new Excetpion(`decode mp3 err: ${err.message}`)
+        }
+    }
+
+    saveForWav(buffer, savePath, filename = '', sampleRate, channels, float = true) {
+
+        if (!filename) filename = 'temp-' + utils.generateTimestampRandom()
+        const fileFullName = filename + '.wav'
+        const fileFullPath = path.join(savePath, fileFullName)
+
+        try {
+            const wavData = wav.encode(buffer, { sampleRate: sampleRate, float: float, channels: channels })
+            utils.saveToPath(fileFullPath, wavData)
+            return fileFullPath
+        } catch (err) {
+            throw new Error(`saveForWav err: ${err.message}`)
+        }
+    }
 }
 
 module.exports = Mp32Wav
--- libs/utils.js
+++ libs/utils.js
@@ -5,164 +5,164 @@ const path = require('path')
 
 module.exports = {
 
-  isArray(obj) {
-    return obj && Object.prototype.toString.call(obj) === '[object Array]'
-  },
+    isArray(obj) {
+        return obj && Object.prototype.toString.call(obj) === '[object Array]'
+    },
 
-  isObj(obj) {
-    return obj && Object.prototype.toString.call(obj) === '[object Object]'
-  },
+    isObj(obj) {
+        return obj && Object.prototype.toString.call(obj) === '[object Object]'
+    },
 
-  /**
-   * check not empty for object
-   * @param obj
-   * @returns {boolean}
-   */
-  objectNotEmpty(obj) {
+    /**
+     * check not empty for object
+     * @param obj
+     * @returns {boolean}
+     */
+    objectNotEmpty(obj) {
 
-    if (this.isObj(obj)) {
-      for (let key in obj) {
-        return true
-      }
-      return false
-    } else {
-      return true
-    }
-  },
+        if (this.isObj(obj)) {
+            for (let key in obj) {
+                return true
+            }
+            return false
+        } else {
+            return true
+        }
+    },
 
-  /**
-   * check not empty for array
-   * @param array
-   * @returns {boolean}
-   */
-  arrayNotEmpty(array) {
+    /**
+     * check not empty for array
+     * @param array
+     * @returns {boolean}
+     */
+    arrayNotEmpty(array) {
 
-    if (this.isArray()) {
-      return array.length > 0
-    } else {
-      return true
-    }
-  },
+        if (this.isArray()) {
+            return array.length > 0
+        } else {
+            return true
+        }
+    },
 
-  /**
-   * 非空(有东西)判断 ps:为空情况：空字符串'',空数组[],空对象{},undefined,null,NaN
-   * @param val
-   * @returns {*|boolean}
-   */
-  judgeNotNull(val) {
-    return (val || val === 0 || val === false) && this.objectNotEmpty(val) && this.arrayNotEmpty(val)
-  },
+    /**
+     * 非空(有东西)判断 ps:为空情况：空字符串'',空数组[],空对象{},undefined,null,NaN
+     * @param val
+     * @returns {*|boolean}
+     */
+    judgeNotNull(val) {
+        return (val || val === 0 || val === false) && this.objectNotEmpty(val) && this.arrayNotEmpty(val)
+    },
 
-  getType(obj) {
-    //tostring会返回对应不同的标签的构造函数
-    var toString = Object.prototype.toString;
-    var map = {
-      '[object Boolean]': 'boolean',
-      '[object Number]': 'number',
-      '[object String]': 'string',
-      '[object Function]': 'function',
-      '[object AsyncFunction]': 'function',
-      '[object Array]': 'array',
-      '[object Date]': 'date',
-      '[object RegExp]': 'regExp',
-      '[object Undefined]': 'undefined',
-      '[object Null]': 'null',
-      '[object Object]': 'object'
-    };
-    var val = toString.call(obj)
-    return map[val];
-  },
+    getType(obj) {
+        //tostring会返回对应不同的标签的构造函数
+        var toString = Object.prototype.toString;
+        var map = {
+            '[object Boolean]': 'boolean',
+            '[object Number]': 'number',
+            '[object String]': 'string',
+            '[object Function]': 'function',
+            '[object AsyncFunction]': 'function',
+            '[object Array]': 'array',
+            '[object Date]': 'date',
+            '[object RegExp]': 'regExp',
+            '[object Undefined]': 'undefined',
+            '[object Null]': 'null',
+            '[object Object]': 'object'
+        };
+        var val = toString.call(obj)
+        return map[val];
+    },
 
-  /**
-   * use reflection tech to check algorithm name exists
-   * @param func_name
-   * @param target_object
-   * @returns {boolean}
-   */
-  checkFuncExists(func_name, target_object) {
+    /**
+     * use reflection tech to check algorithm name exists
+     * @param func_name
+     * @param target_object
+     * @returns {boolean}
+     */
+    checkFuncExists(func_name, target_object) {
 
-    if (this.judgeNotNull(func_name) && this.judgeNotNull(target_object)) {
-      const func = target_object[func_name]
-      return this.getType(func) === 'function'
-    } else {
-      return false
-    }
-  },
+        if (this.judgeNotNull(func_name) && this.judgeNotNull(target_object)) {
+            const func = target_object[func_name]
+            return this.getType(func) === 'function'
+        } else {
+            return false
+        }
+    },
 
-  /**
-   * detect null arguments
-   * @param args
-   * @returns {boolean}
-   */
-  checkArgsNotNull(...args) {
+    /**
+     * detect null arguments
+     * @param args
+     * @returns {boolean}
+     */
+    checkArgsNotNull(...args) {
 
-    let res = true
+        let res = true
 
-    args.forEach(async val => {
-      if (!this.judgeNotNull(val)) {
-        res = false
-        return false
-      }
-    })
-    return res
-  },
+        args.forEach(async val => {
+            if (!this.judgeNotNull(val)) {
+                res = false
+                return false
+            }
+        })
+        return res
+    },
 
-  readFile(filePath) {
+    readFile(filePath) {
 
-    if (filePath && fs.pathExistsSync(filePath)) {
-      return fs.readFileSync(filePath)
-    }
-  },
+        if (filePath && fs.pathExistsSync(filePath.slice('\'').join('/'))) {
+            return fs.readFileSync(filePath.slice('\'').join('/'))
+        }
+    },
 
-  splitFilename(path) {
+    splitFilename(path) {
 
-    let split = path.split('/')
-    return split.slice(-1)
-  },
+        let split = path.split('/')
+        return split.slice(-1)
+    },
 
-  splitFileDir(path) {
+    splitFileDir(path) {
 
-    let split = path.split('/')
-    split.splice(-1, 1)
-    return split.join('/')
-  },
+        let split = path.split('/')
+        split.splice(-1, 1)
+        return split.join('/')
+    },
 
-  /**
-   * ensure dir splitting last former "/" path
-   * @param dirPath
-   */
-  ensurePathExists(dirPath) {
+    /**
+     * ensure dir splitting last former "/" path
+     * @param dirPath
+     */
+    ensurePathExists(dirPath) {
 
-    if (!dirPath) return
-    let splitDirPath = dirPath.split('/')
-    if (splitDirPath.length > 1) {
-      splitDirPath.pop()
-      splitDirPath = splitDirPath.join('/')
-    }
-    if (!fs.pathExistsSync(splitDirPath)) {
-      fs.mkdirpSync(splitDirPath)
-    }
-  },
+        if (!dirPath) return
+        let splitDirPath = dirPath.slice('\'').join('/').split('/')
+        if (splitDirPath.length > 1) {
+            splitDirPath.pop()
+            splitDirPath = splitDirPath.join('/')
+        }
+        if (!fs.pathExistsSync(splitDirPath)) {
+            fs.mkdirpSync(splitDirPath)
+        }
+    },
 
-  saveToPath(fullPath, data) {
+    saveToPath(fullPath, data) {
 
-    this.ensurePathExists(fullPath)
-    fs.writeFileSync(fullPath, data)
-  },
+        this.ensurePathExists(fullPath.slice('\'').join('/'))
+        fs.writeFileSync(fullPath.slice('\'').join('/'), data)
+    },
 
-  generateRandomNum(num = 10) {
+    generateRandomNum(num = 10) {
 
-    // 方法1
-    let res = 0
-    for (let i = 1; i <= num; i++) {
-      res += Math.round(Math.random() * num) * Math.pow(num, i);
-    }
-    return res
-    // 方法2
-    // return Math.random().toString().slice(-num)
-  },
+        // 方法1
+        let res = 0
+        for (let i = 1; i <= num; i++) {
+            res += Math.round(Math.random() * num) * Math.pow(num, i);
+        }
+        return res
+            // 方法2
+            // return Math.random().toString().slice(-num)
+    },
 
-  generateTimestampRandom(splitDirPathFlag = '-') {
-    return new Date().getTime() + splitDirPathFlag + this.generateRandomNum(6)
-  },
+    generateTimestampRandom(splitDirPathFlag = '-') {
+        return new Date().getTime() + splitDirPathFlag + this.generateRandomNum(6)
+    },
 }
